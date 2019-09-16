980c6899778ffb4be3804641d87b85f37748a76c
Michael James <mikeyj777@gmail.com>
2019-09-15 23:36:26 -0400

    finishing up frog problem & added amazon staircase problem

--- Frog_Problem/frog_hops.py
+++ Frog_Problem/frog_hops.py
@@ -3,20 +3,40 @@
 # is there a general formula for n lily pads?
 
 import random
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-def jumper(spacesleft=10, numjumps=0):
+def jumper(spacesleft=20, numjumps=0):
     if spacesleft <= 0:
         return numjumps
 
     jump = random.randint(1, spacesleft)
+#     print("spaces left: ", spacesleft, ". jump: ",
+#           jump, "num jumps: ", numjumps + 1)
+    return jumper(spacesleft - jump, numjumps + 1)
 
-    jumper(spacesleft - jump, numjumps + 1)
 
-    return None
+def looper(epochs=1000, spacesleft=20):
+    spaces = 3
+    meansvsspaces = []
+    while spaces <= spacesleft:
+        alljumps = []
+        i = 1
+        while i <= epochs:
+            #         print("_______")
+            currjumps = jumper(spacesleft=spaces, numjumps=0)
+            alljumps.append(currjumps)
+            i += 1
+        meansvsspaces.append([spaces, np.mean(alljumps)])
+        spaces += 1
+
+    return meansvsspaces
 
 
-def looper(epochs=10):
-    alljumps = []
-    currjumps = jumper()
-    alljumps.append(currjumps)
+a = looper()
+a = np.asarray(a)
+# print(np.mean(a))
+print(a)
+plt.plot(a[:, 0], a[:, 1])
+plt.show()
